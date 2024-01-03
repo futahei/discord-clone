db909356676c4b6e396d795135a0e02171542d58
Futahei <futahei1108@icloud.com>
2024-01-03 15:58:16 +0900

    フォルダ構成

---
+++ app/(main)/(routes)/page.tsx
@@ -0,0 +1,8 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+
+const state = false;
+
+export default function Home() {
+  return <div className="">This is a protected route.</div>;
+}
