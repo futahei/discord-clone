ae1f4e00ba9751de20f7b50dfadf53195b06194b
Futahei <futahei1108@icloud.com>
2024-01-21 17:57:46 +0900

    サイドバーを追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
-            enableSystem={false}
+            enableSystem={true}
             storageKey="discord-theme"
           >
             {children}
